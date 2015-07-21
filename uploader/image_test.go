dbe996d450d16cd58074bb224b7b248ad001734b
Rafal Piekarski <ravbaker@gmail.com>
2015-07-21 18:26:54 +0200

    basic tests for 2 files

---
+++ uploader/image_test.go
@@ -0,0 +1,14 @@
+package uploader
+
+import (
+  "testing"
+)
+
+func TestJsonResponse(t *testing.T) {
+  image := Image { "http://localhost:8080/image/123.png", "sample.png", 42 }
+  var expectedJsonResponse string = `{"link":"http://localhost:8080/image/123.png","filename":"sample.png","Time":42}`
+  jsonResponse := image.Json()
+  if jsonResponse != expectedJsonResponse {
+    t.Errorf("Json response doesn't match requred pattern: '%v' expected: '%v'", jsonResponse, expectedJsonResponse)
+  }
+}
