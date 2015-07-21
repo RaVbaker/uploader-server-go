de89ff4e6585fdb12bce17c065e6462b58c9f419
Rafal Piekarski <ravbaker@gmail.com>
2015-07-21 18:27:37 +0200

    :lipstick: proper formatting for tests

--- uploader/file_upload_test.go
+++ uploader/file_upload_test.go
@@ -1,29 +1,29 @@
 package uploader
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestFileExtensionDetection(t *testing.T) {
-  var filetypeToExtension map[string]string
-  filetypeToExtension = map[string]string {
-    "image/jpeg": "jpg",
-    "image/jpg": "jpg",
-    "image/png": "png",
-    "image/gif": "gif",
-  }
+	var filetypeToExtension map[string]string
+	filetypeToExtension = map[string]string{
+		"image/jpeg": "jpg",
+		"image/jpg":  "jpg",
+		"image/png":  "png",
+		"image/gif":  "gif",
+	}
 
-  for filetype, expectedExtension := range filetypeToExtension {
-    foundExtension, _ := fileExtension(filetype)
-    if foundExtension != expectedExtension {
-      t.Errorf("Didn't detect proper extension from %s. Found: %v", filetype, foundExtension)
-    }
-  }
+	for filetype, expectedExtension := range filetypeToExtension {
+		foundExtension, _ := fileExtension(filetype)
+		if foundExtension != expectedExtension {
+			t.Errorf("Didn't detect proper extension from %s. Found: %v", filetype, foundExtension)
+		}
+	}
 }
 
 func TestFileExtensionDetectionFailed(t *testing.T) {
-  _, err := fileExtension("unknown/type")
-  if err == nil {
-    t.Fatal("We should return error when extension not detected!")
-  }
+	_, err := fileExtension("unknown/type")
+	if err == nil {
+		t.Fatal("We should return error when extension not detected!")
+	}
 }
--- uploader/image_test.go
+++ uploader/image_test.go
@@ -1,14 +1,14 @@
 package uploader
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestJsonResponse(t *testing.T) {
-  image := Image { "http://localhost:8080/image/123.png", "sample.png", 42 }
-  var expectedJsonResponse string = `{"link":"http://localhost:8080/image/123.png","filename":"sample.png","Time":42}`
-  jsonResponse := image.Json()
-  if jsonResponse != expectedJsonResponse {
-    t.Errorf("Json response doesn't match requred pattern: '%v' expected: '%v'", jsonResponse, expectedJsonResponse)
-  }
+	image := Image{"http://localhost:8080/image/123.png", "sample.png", 42}
+	var expectedJsonResponse string = `{"link":"http://localhost:8080/image/123.png","filename":"sample.png","Time":42}`
+	jsonResponse := image.Json()
+	if jsonResponse != expectedJsonResponse {
+		t.Errorf("Json response doesn't match requred pattern: '%v' expected: '%v'", jsonResponse, expectedJsonResponse)
+	}
 }
