3bce8e789939aaf1ef8bb89acfcdf6a88c0f0b68
Rafal Piekarski <ravbaker@gmail.com>
2015-07-22 10:10:47 +0200

    :lipstick: for new files: images.go & images_test.gp

--- uploader/images.go
+++ uploader/images.go
@@ -1,29 +1,28 @@
 package uploader
 
 type ImagesCollection struct {
-  List []*Image
+	List []*Image
 }
 
 var Images ImagesCollection
 
-func (images *ImagesCollection) Iter () <- chan *Image {
-    ch := make(chan *Image);
-    size := len(images.List)
-    go func () {
-        for i := 0; i < size; i++ {
-            ch <- images.List[i]
-        }
-        close(ch)
-    } ()
-    return ch
+func (images *ImagesCollection) Iter() <-chan *Image {
+	ch := make(chan *Image)
+	size := len(images.List)
+	go func() {
+		for i := 0; i < size; i++ {
+			ch <- images.List[i]
+		}
+		close(ch)
+	}()
+	return ch
 }
 
 func SaveImage(image *Image) {
-  Images.List = append(Images.List, image)
+	Images.List = append(Images.List, image)
 }
 
-
 func init() {
-  Images := new(ImagesCollection)
-  Images.List = make([]*Image, 10)
+	Images := new(ImagesCollection)
+	Images.List = make([]*Image, 10)
 }
--- uploader/images_test.go
+++ uploader/images_test.go
@@ -1,71 +1,69 @@
 package uploader
 
 import (
-  _ "fmt"
-  "testing"
+	_ "fmt"
+	"testing"
 )
 
 func TestImageSaving(t *testing.T) {
-  Images.List = Images.List[:0]
+	Images.List = Images.List[:0]
 
-  img1 := NewImage("foo/bar", "img1")
-  img2 := NewImage("bar/baz", "img2")
-  SaveImage(img1)
-  SaveImage(img2)
+	img1 := NewImage("foo/bar", "img1")
+	img2 := NewImage("bar/baz", "img2")
+	SaveImage(img1)
+	SaveImage(img2)
 
-  if len(Images.List) != 2 {
-    t.Error("Didn't manage to save both images")
-  }
+	if len(Images.List) != 2 {
+		t.Error("Didn't manage to save both images")
+	}
 
-  if Images.List[1].Filename != "img2" {
-    t.Error("Didn't manage to retrieve ")
-  }
+	if Images.List[1].Filename != "img2" {
+		t.Error("Didn't manage to retrieve ")
+	}
 }
-
 
 func TestImageSavingFromGoroutine(t *testing.T) {
-  Images.List = Images.List[:0]
+	Images.List = Images.List[:0]
 
-  img1 := NewImage("foo/bar", "img1")
-  img2 := NewImage("bar/baz", "img2")
-  done := make(chan bool)
+	img1 := NewImage("foo/bar", "img1")
+	img2 := NewImage("bar/baz", "img2")
+	done := make(chan bool)
 
-  go func() {
-    SaveImage(img1)
-    done <- true
-  }()
-  SaveImage(img2)
+	go func() {
+		SaveImage(img1)
+		done <- true
+	}()
+	SaveImage(img2)
 
-  <- done
+	<-done
 
-  if len(Images.List) != 2 {
-    t.Error("Didn't manage to save both images. Found only:", len(Images.List))
-  }
+	if len(Images.List) != 2 {
+		t.Error("Didn't manage to save both images. Found only:", len(Images.List))
+	}
 
-  if Images.List[1].Filename != "img2" && Images.List[1].Filename != "img1" { // because we don't know the order
-    t.Error("Didn't manage to retrieve ", Images.List[1].Filename)
-  }
+	if Images.List[1].Filename != "img2" && Images.List[1].Filename != "img1" { // because we don't know the order
+		t.Error("Didn't manage to retrieve ", Images.List[1].Filename)
+	}
 }
 
-
 func TestIteratingThroughImagesList(t *testing.T) {
-  Images.List = Images.List[:0]
+	Images.List = Images.List[:0]
 
-  SaveImage(&Image{ "foo/bar", "img1", 1})
-  SaveImage(&Image{ "foo/bar", "img2", 2})
-  SaveImage(&Image{ "foo/bar", "img2", 4})
-  const expectedCounter = 7
+	SaveImage(&Image{"foo/bar", "img1", 1})
+	SaveImage(&Image{"foo/bar", "img2", 2})
+	SaveImage(&Image{"foo/bar", "img2", 4})
+	const expectedCounter = 7
 
-  if len(Images.List) != 3 {
-    t.Error("Didn't save all 3 images")
-  }
+	if len(Images.List) != 3 {
+		t.Error("Didn't save all 3 images")
+	}
 
-  var counter int;
-  for image := range Images.Iter() {
-    counter = counter + int(image.Time)
-  }
+	var counter int
+	for image := range Images.Iter() {
+		counter = counter + int(image.Time)
+	}
 
-  if counter != expectedCounter {
-    t.Error("Iterator failed to iterate through all elements. counter:", counter)
-  }
+	if counter != expectedCounter {
+		t.Error("Iterator failed to iterate through all elements. counter:", counter)
+	}
 }
